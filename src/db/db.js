326a4f20a17ba67219e0e0b072291cae7fd393f2
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2025-10-28 08:43:38 +0100

    feat: add database config

--- src/db/db.js
+++ src/db/db.js
@@ -1,15 +1,16 @@
-import mysql from 'mysql2/promise';
+import knex from 'knex';
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+const db = knex({
+  client: 'mysql2',
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+  pool: { min: 0, max: 10 },
 });
 
-
+export default db;
